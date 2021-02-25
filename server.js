1ab1845e2a3e4ec4f6f1f2415f9364db3dd76262
hitnei <hoangnhi050298@gmail.com>
2021-02-25 21:58:42 +0700

    .

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 
 const routes = require('./routes');
 
-express.set('views', path.join(__dirname, 'client'))
+app.set('views', path.join(__dirname, 'client'))
 
 // require db connection
 require('./models');
